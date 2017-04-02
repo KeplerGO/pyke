f8afb1941dc0f4fb027e72744ad611c014b645f8
Ze Vinicius <jvmirca@gmail.com>
2017-04-02 02:20:22 -0700

    fix kepdraw

--- kepdraw.py
+++ kepdraw.py
@@ -1,7 +1,6 @@
-
-import pylab, numpy
-from pylab import *
-from matplotlib import *
+import numpy as np
+import sys
+from matplotlib import pyplot as plt
 import kepio, kepmsg, kepkey
 import re
 
@@ -12,9 +11,9 @@ __url__ = "$URL: svn+ssh://mstill@murzim.amn.nasa.gov/data-repo/trunk/data/fligh
 def kepdraw(infile,outfile,datacol,ploterr,errcol,quality,
 	    lcolor,lwidth,fcolor,falpha,labelsize,ticksize,
 	    xsize,ysize,fullrange,chooserange,y1,y2,plotgrid,
-            ylabel,plottype,verbose,logfile,status,cmdLine=False): 
+            ylabel,plottype,verbose,logfile,status,cmdLine=False):
 
-# log the call 
+# log the call
 
     hashline = '----------------------------------------------------------------------------'
     kepmsg.log(logfile,hashline,verbose)
@@ -93,17 +92,17 @@ def kepdraw(infile,outfile,datacol,ploterr,errcol,quality,
 # remove infinities and bad data
 
     if status == 0:
-        if numpy.isnan(numpy.nansum(indataerr)):
+        if np.isnan(np.nansum(indataerr)):
             indataerr[:] = 1.0e-5
-        work1 = numpy.array([intime, indata, indataerr, qualty],dtype='float64')
-        work1 = numpy.rot90(work1,3)
-        work1 = work1[~numpy.isnan(work1).any(1)]
-        work1 = work1[~numpy.isinf(work1).any(1)]
+        work1 = np.array([intime, indata, indataerr, qualty],dtype='float64')
+        work1 = np.rot90(work1,3)
+        work1 = work1[~np.isnan(work1).any(1)]
+        work1 = work1[~np.isinf(work1).any(1)]
         if quality:
             work1 = work1[work1[:,0] == 0.0]
-        barytime = numpy.array(work1[:,3],dtype='float64')
-        data = numpy.array(work1[:,2],dtype='float32')
-        dataerr = numpy.array(work1[:,1],dtype='float32')
+        barytime = np.array(work1[:,3],dtype='float64')
+        data = np.array(work1[:,2],dtype='float32')
+        dataerr = np.array(work1[:,1],dtype='float32')
         if len(barytime) == 0:
             message = 'ERROR -- KEPDRAW: Plotting arrays are full of NaN'
             status = kepmsg.err(logfile,message,verbose)
@@ -119,7 +118,7 @@ def kepdraw(infile,outfile,datacol,ploterr,errcol,quality,
 
         nrm = 0
         try:
-            nrm = len(str(int(numpy.nanmax(data))))-1
+            nrm = len(str(int(np.nanmax(data))))-1
         except:
             nrm = 0
 	data = data / 10**nrm
@@ -130,20 +129,19 @@ def kepdraw(infile,outfile,datacol,ploterr,errcol,quality,
                 ylab1 = '10$^{%d}$ e$^-$ s$^{-1}$' % nrm
         else:
             ylab1 = re.sub('_','-',ylabel)
-            
 
 # data limits
 
-	xmin = numpy.nanmin(barytime)
-	xmax = numpy.nanmax(barytime)
-	ymin = numpy.nanmin(data)
-	ymax = numpy.nanmax(data)
+	xmin = np.nanmin(barytime)
+	xmax = np.nanmax(barytime)
+	ymin = np.nanmin(data)
+	ymax = np.nanmax(data)
 	xr = xmax - xmin
 	yr = ymax - ymin
-        barytime = insert(barytime,[0],[barytime[0]]) 
-        barytime = append(barytime,[barytime[-1]])
-        data = insert(data,[0],[-10000.0]) 
-        data = append(data,-10000.0)
+        barytime = np.insert(barytime,[0],[barytime[0]])
+        barytime = np.append(barytime,[barytime[-1]])
+        data = np.insert(data,[0],[-10000.0])
+        data = np.append(data,-10000.0)
 
 
 # define plot formats
@@ -158,111 +156,81 @@ def kepdraw(infile,outfile,datacol,ploterr,errcol,quality,
                       'legend.fontsize': 12,
                       'xtick.labelsize': ticksize,
                       'ytick.labelsize': ticksize}
-            pylab.rcParams.update(params)
+            plt.rcParams.update(params)
         except:
             pass
 
 # define size of plot on monitor screen
 
-	pylab.figure(figsize=[xsize,ysize])
+	plt.figure(figsize=[xsize,ysize])
 
 # delete any fossil plots in the matplotlib window
 
-        pylab.clf()
+        plt.clf()
 
 # position axes inside the plotting window
 
-#        ax = pylab.axes([0.1,0.11,0.89,0.87])
-	ax = pylab.subplot(111)
-	pylab.subplots_adjust(0.06,0.15,0.92,0.83)
+#        ax = plt.axes([0.1,0.11,0.89,0.87])
+	ax = plt.subplot(111)
+	plt.subplots_adjust(0.06,0.15,0.92,0.83)
 
 # force tick labels to be absolute rather than relative
 
-        pylab.gca().xaxis.set_major_formatter(pylab.ScalarFormatter(useOffset=False))
-        pylab.gca().yaxis.set_major_formatter(pylab.ScalarFormatter(useOffset=False))
-        ax.yaxis.set_major_locator(MaxNLocator(5))
+        plt.gca().xaxis.set_major_formatter(plt.ScalarFormatter(useOffset=False))
+        plt.gca().yaxis.set_major_formatter(plt.ScalarFormatter(useOffset=False))
+        ax.yaxis.set_major_locator(plt.MaxNLocator(5))
 
 # rotate y labels by 90 deg
 
         labels = ax.get_yticklabels()
-        setp(labels, 'rotation', 90, fontsize=ticksize)
+        plt.setp(labels, 'rotation', 90, fontsize=ticksize)
 
 # if plot type is 'fast' plot data time series as points
 
         if plottype == 'fast':
-            pylab.plot(barytime,data,'o',color=lcolor)            
+            plt.plot(barytime,data,'o',color=lcolor)
 
 # if plot type is 'pretty' plot data time series as an unbroken line, retaining data gaps
 
         else:
-            ltime = numpy.array([],dtype='float64')
-            ldata = numpy.array([],dtype='float32')
+            ltime = np.array([],dtype='float64')
+            ldata = np.array([],dtype='float32')
             dt = 0
             work1 = 2.0 * cadence / 86400
             for i in range(1,len(data)-1):
                 dt = barytime[i] - barytime[i-1]
                 if dt < work1:
-                    ltime = numpy.append(ltime,barytime[i])
-                    ldata = numpy.append(ldata,data[i])
+                    ltime = np.append(ltime,barytime[i])
+                    ldata = np.append(ldata,data[i])
                 else:
-                    pylab.plot(ltime,ldata,color=lcolor,linestyle='-',linewidth=lwidth)
-                    ltime = numpy.array([],dtype='float64')
-                    ldata = numpy.array([],dtype='float32')
-            pylab.plot(ltime,ldata,color=lcolor,linestyle='-',linewidth=lwidth)
+                    plt.plot(ltime,ldata,color=lcolor,linestyle='-',linewidth=lwidth)
+                    ltime = np.array([],dtype='float64')
+                    ldata = np.array([],dtype='float32')
+            plt.plot(ltime,ldata,color=lcolor,linestyle='-',linewidth=lwidth)
 
 # plot the fill color below data time series, with no data gaps
 
-	pylab.fill(barytime,data,fc=fcolor,linewidth=0.0,alpha=falpha)
+	plt.fill(barytime,data,fc=fcolor,linewidth=0.0,alpha=falpha)
 
 # define plot x and y limits
 
-	pylab.xlim(xmin-xr*0.01,xmax+xr*0.01)
+	plt.xlim(xmin-xr*0.01,xmax+xr*0.01)
 	if ymin-yr*0.01 <= 0.0 or fullrange:
-            pylab.ylim(1.0e-10,ymax+yr*0.01)
+            plt.ylim(1.0e-10,ymax+yr*0.01)
 	else:
-            pylab.ylim(ymin-yr*0.01,ymax+yr*0.01)
+            plt.ylim(ymin-yr*0.01,ymax+yr*0.01)
         if chooserange:
-            pylab.ylim(y1,y2)
+            plt.ylim(y1,y2)
 
 # plot labels
 
-	pylab.xlabel(xlab, {'color' : 'k'})
+	plt.xlabel(xlab, {'color' : 'k'})
         try:
-            pylab.ylabel(ylab1, {'color' : 'k'})
+            plt.ylabel(ylab1, {'color' : 'k'})
         except:
             ylab1 = '10**%d e-/s' % nrm
-            pylab.ylabel(ylab1, {'color' : 'k'})
+            plt.ylabel(ylab1, {'color' : 'k'})
 
-# make grid on plot
-
-#	if plotgrid: pylab.grid()
-
-# TEMPORARY !!!!!!!!!!!!!!!!!!!
-
-#        btime = numpy.arange(barytime[0],barytime[-1],0.25) + 0.125
-#        bflux = numpy.zeros((len(btime)))
-#        j = 0
-#        work = numpy.array([])
-#        for i in range(1,len(barytime)-1):
-#            if barytime[i] >= btime[j] - 0.125 and barytime[i] < btime[j] + 0.125:
-#                work = numpy.append(work,data[i])
-#            else:
-#                bflux[j] = numpy.mean(work)
-#                work = numpy.array([])
-#                j += 1
-#        bflux[j] = numpy.mean(work)
-#                
-#        pylab.plot(btime,bflux,color='r',linestyle='',marker='D',markersize=20)
-#	print numpy.std(bflux)        
-#
-#        pylab.plot([0.0,10000.0],[-49.5,-49.5],color='k',linestyle='--',linewidth=2.0)
-#        pylab.plot([0.0,10000.0],[49.5,49.5],color='k',linestyle='--',linewidth=2.0)
-##        pylab.plot([0.0,10000.0],[15.5,15.5],color='k',linestyle=':',linewidth=4.0)
-##        pylab.plot([0.0,10000.0],[-15.5,-15.5],color='k',linestyle=':',linewidth=4.0)
-##        pylab.plot([0.0,10000.0],[-202,-202],color='k',linestyle='--',linewidth=2.0)
-##        pylab.plot([0.0,10000.0],[202,202],color='k',linestyle='--',linewidth=2.0)
-##        pylab.plot([0.0,10000.0],[0,0],color='k',linestyle=':',linewidth=4.0)
-##        pylab.plot([0.0,10000.0],[-81.*12.3,-81.*12.3],color='k',linestyle=':',linewidth=4.0)
         ax.minorticks_on()
         ax.tick_params('both', length=20, width=2, which='major')
         ax.tick_params('both', length=10, width=1, which='minor')
@@ -270,17 +238,10 @@ def kepdraw(infile,outfile,datacol,ploterr,errcol,quality,
 # save plot to file
 
     if status == 0 and outfile.lower() != 'none':
-	pylab.savefig(outfile)
+	plt.savefig(outfile)
 
 # render plot
-
-        if cmdLine: 
-            pylab.show(block=True)
-        else: 
-            pylab.ion()
-            pylab.plot([])
-            pylab.ioff()
-	
+        plt.show(block=True)
 # end time
 
     if (status == 0):
@@ -294,7 +255,7 @@ def kepdraw(infile,outfile,datacol,ploterr,errcol,quality,
 
 if '--shell' in sys.argv:
     import argparse
-    
+
     parser = argparse.ArgumentParser(description='Interactive plotting of Kepler time series data')
     parser.add_argument('--shell', action='store_true', help='Are we running from the shell?')
 
@@ -331,7 +292,6 @@ if '--shell' in sys.argv:
             args.xsize,args.ysize,args.fullrange,args.chooserange,args.ymin,args.ymax,
             args.plotgrid,args.ylabel,args.plottype,args.verbose,args.logfile,args.status,
             cmdLine)
-    
 
 else:
     from pyraf import iraf
