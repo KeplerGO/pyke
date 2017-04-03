75d6592e7de8a8a921fe67915ee3e56b09eaa8ce
Ze Vinicius <jvmirca@gmail.com>
2017-04-03 11:06:17 -0700

    update test_imports

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,7 +1,14 @@
 def test_import():
-    from ..kepclip import kepclip
+    from ..kepclip    import kepclip
     from ..kepoutlier import kepoutlier
     from ..kepcotrend import kepcotrendsc
     from ..kepdetrend import kepdetrend
-    from ..kepdiffim import kepdiffim
+    from ..kepdiffim  import kepdiffim
     from ..kepextract import kepextract
+    from ..kepft      import kepft
+    from ..kepmask    import kepmask
+    from ..kepflatten import kepflatten
+    from ..kepfit     import *
+    from ..kepfunc    import *
+    from ..kepsmooth  import kepsmooth
+    from ..kepstddev  import kepstddev
