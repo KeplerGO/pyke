eeac4af0f88f41ed6245d6e6c2071ca486218c15
Ze Vinicius <jvmirca@gmail.com>
2017-04-04 15:37:29 -0700

    fix keptrim

--- keptrim.py
+++ keptrim.py
@@ -1,17 +1,17 @@
-import numpy, sys, time, pylab, urllib
-from numpy import *
+import time, urllib
+import sys
+import numpy as np
 from astropy.io import fits as pyfits
-from pylab import *
-from matplotlib import *
+from matplotlib import pyplot as plt
 import kepio, kepmsg, kepkey, kepstat
 
-def keptrim(infile,outfile,kepid,column,row,imsize,clobber,verbose,logfile,status): 
+def keptrim(infile,outfile,kepid,column,row,imsize,clobber,verbose,logfile,status):
 
 # startup parameters
 
     status = 0
 
-# log the call 
+# log the call
 
     hashline = '----------------------------------------------------------------------------'
     kepmsg.log(logfile,hashline,verbose)
@@ -42,7 +42,7 @@ def keptrim(infile,outfile,kepid,column,row,imsize,clobber,verbose,logfile,statu
 # clobber output file
 
     if clobber: status = kepio.clobber(outfile,logfile,verbose)
-    if kepio.fileexists(outfile): 
+    if kepio.fileexists(outfile):
         message = 'ERROR -- KEPTRIM: ' + outfile + ' exists. Use --clobber'
         status = kepmsg.err(logfile,message,verbose)
 
@@ -85,7 +85,7 @@ def keptrim(infile,outfile,kepid,column,row,imsize,clobber,verbose,logfile,statu
         crval1p = cards2['CRVAL1P'].value
         crval2p = cards2['CRVAL2P'].value
         cdelt1p = cards2['CDELT1P'].value
-        cdelt2p = cards2['CDELT2P'].value        
+        cdelt2p = cards2['CDELT2P'].value
         imcol = (column - crval1p) * cdelt1p + crpix1p - 1
         imrow = (row - crval2p) * cdelt2p + crpix2p - 1
         crval1p = column - imsize / 2 + 0.5
@@ -132,20 +132,20 @@ def keptrim(infile,outfile,kepid,column,row,imsize,clobber,verbose,logfile,statu
 # reshape time series images
 
     if status == 0:
-        isize = numpy.shape(flux)[0]
-        jsize = numpy.shape(flux)[1]
-        ksize = numpy.shape(flux)[2]
-        raw_cnts = numpy.reshape(raw_cnts,(isize,jsize*ksize))
-        flux = numpy.reshape(flux,(isize,jsize*ksize))
-        flux_err = numpy.reshape(flux_err,(isize,jsize*ksize))
-        flux_bkg = numpy.reshape(flux_bkg,(isize,jsize*ksize))
-        flux_bkg_err = numpy.reshape(flux_bkg_err,(isize,jsize*ksize))
-        cosmic_rays = numpy.reshape(cosmic_rays,(isize,jsize*ksize))
-        
+        isize = np.shape(flux)[0]
+        jsize = np.shape(flux)[1]
+        ksize = np.shape(flux)[2]
+        raw_cnts = np.reshape(raw_cnts,(isize,jsize*ksize))
+        flux = np.reshape(flux,(isize,jsize*ksize))
+        flux_err = np.reshape(flux_err,(isize,jsize*ksize))
+        flux_bkg = np.reshape(flux_bkg,(isize,jsize*ksize))
+        flux_bkg_err = np.reshape(flux_bkg_err,(isize,jsize*ksize))
+        cosmic_rays = np.reshape(cosmic_rays,(isize,jsize*ksize))
+
 # pixel map data
 
     if status == 0:
-        maskmap = array(instr[2].data[y1:y2,x1:x2])
+        maskmap = np.array(instr[2].data[y1:y2,x1:x2])
 
 # construct output primary extension
 
@@ -153,14 +153,14 @@ def keptrim(infile,outfile,kepid,column,row,imsize,clobber,verbose,logfile,statu
         hdu0 = pyfits.PrimaryHDU()
         for i in range(len(cards0)):
             try:
-                if cards0[i].key not in hdu0.header.keys():
-                    hdu0.header.update(cards0[i].key, cards0[i].value, cards0[i].comment)
+                if cards0[i].keyword not in hdu0.header.keys():
+                    hdu0.header[cards0[i].keyword] = (cards0[i].value, cards0[i].comment)
                 else:
-                    hdu0.header.cards[cards0[i].key].comment = cards0[i].comment
+                    hdu0.header.cards[cards0[i].keyword].comment = cards0[i].comment
             except:
                 pass
         status = kepkey.history(call,hdu0,outfile,logfile,verbose)
-        outstr = HDUList(hdu0)
+        outstr = pyfits.HDUList(hdu0)
 
 # construct output light curve extension
 
@@ -169,52 +169,52 @@ def keptrim(infile,outfile,kepid,column,row,imsize,clobber,verbose,logfile,statu
         eformat = str(imsize*imsize) + 'E'
         jformat = str(imsize*imsize) + 'J'
         kformat = str(imsize*imsize) + 'K'
-        col1 = Column(name='TIME',format='D',unit='BJD - 2454833',array=time)
-        col2 = Column(name='TIMECORR',format='E',unit='d',array=timecorr)
-        col3 = Column(name='CADENCENO',format='J',array=cadenceno)
-        col4 = Column(name='RAW_CNTS',format=jformat,unit='count',dim=coldim,array=raw_cnts)
-        col5 = Column(name='FLUX',format=eformat,unit='e-/s',dim=coldim,array=flux)
-        col6 = Column(name='FLUX_ERR',format=eformat,unit='e-/s',dim=coldim,array=flux_err)
-        col7 = Column(name='FLUX_BKG',format=eformat,unit='e-/s',dim=coldim,array=flux_bkg)
-        col8 = Column(name='FLUX_BKG_ERR',format=eformat,unit='e-/s',dim=coldim,array=flux_bkg_err)
-        col9 = Column(name='COSMIC_RAYS',format=eformat,unit='e-/s',dim=coldim,array=cosmic_rays)
-        col10 = Column(name='QUALITY',format='J',array=quality)
-        col11 = Column(name='POS_CORR1',format='E',unit='pixel',array=pos_corr1)
-        col12 = Column(name='POS_CORR2',format='E',unit='pixel',array=pos_corr2)
-        cols = ColDefs([col1,col2,col3,col4,col5,col6,col7,col8,col9,col10,col11,col12])
-        hdu1 = new_table(cols)
+        col1 =  pyfits.Column(name='TIME',format='D',unit='BJD - 2454833',array=time)
+        col2 =  pyfits.Column(name='TIMECORR',format='E',unit='d',array=timecorr)
+        col3 =  pyfits.Column(name='CADENCENO',format='J',array=cadenceno)
+        col4 =  pyfits.Column(name='RAW_CNTS',format=jformat,unit='count',dim=coldim,array=raw_cnts)
+        col5 =  pyfits.Column(name='FLUX',format=eformat,unit='e-/s',dim=coldim,array=flux)
+        col6 =  pyfits.Column(name='FLUX_ERR',format=eformat,unit='e-/s',dim=coldim,array=flux_err)
+        col7 =  pyfits.Column(name='FLUX_BKG',format=eformat,unit='e-/s',dim=coldim,array=flux_bkg)
+        col8 =  pyfits.Column(name='FLUX_BKG_ERR',format=eformat,unit='e-/s',dim=coldim,array=flux_bkg_err)
+        col9 =  pyfits.Column(name='COSMIC_RAYS',format=eformat,unit='e-/s',dim=coldim,array=cosmic_rays)
+        col10 = pyfits.Column(name='QUALITY',format='J',array=quality)
+        col11 = pyfits.Column(name='POS_CORR1',format='E',unit='pixel',array=pos_corr1)
+        col12 = pyfits.Column(name='POS_CORR2',format='E',unit='pixel',array=pos_corr2)
+        cols =  pyfits.ColDefs([col1,col2,col3,col4,col5,col6,col7,col8,col9,col10,col11,col12])
+        hdu1 =  pyfits.BinTableHDU.from_columns(cols)
         for i in range(len(cards1)):
             try:
-                if cards1[i].key not in hdu1.header.keys():
-                    hdu1.header.update(cards1[i].key, cards1[i].value, cards1[i].comment)
+                if cards1[i].keyword not in hdu1.header.keys():
+                    hdu1.header[cards1[i].keyword] = (cards1[i].value, cards1[i].comment)
                 else:
-                    hdu1.header.cards[cards1[i].key].comment = cards1[i].comment
+                    hdu1.header.cards[cards1[i].keyword].comment = cards1[i].comment
             except:
                 pass
-        hdu1.header.update('1CRV4P',crval1p,'[pixel] detector coordinate at reference pixel')
-        hdu1.header.update('2CRV4P',crval2p,'[pixel] detector coordinate at reference pixel')
-        hdu1.header.update('1CRPX4',(imsize + 1) / 2,'[pixel] reference pixel along image axis 1')
-        hdu1.header.update('2CRPX4',(imsize + 1) / 2,'[pixel] reference pixel along image axis 2')
-        hdu1.header.update('1CRV5P',crval1p,'[pixel] detector coordinate at reference pixel')
-        hdu1.header.update('2CRV5P',crval2p,'[pixel] detector coordinate at reference pixel')
-        hdu1.header.update('1CRPX5',(imsize + 1) / 2,'[pixel] reference pixel along image axis 1')
-        hdu1.header.update('2CRPX5',(imsize + 1) / 2,'[pixel] reference pixel along image axis 2')
-        hdu1.header.update('1CRV6P',crval1p,'[pixel] detector coordinate at reference pixel')
-        hdu1.header.update('2CRV6P',crval2p,'[pixel] detector coordinate at reference pixel')
-        hdu1.header.update('1CRPX6',(imsize + 1) / 2,'[pixel] reference pixel along image axis 1')
-        hdu1.header.update('2CRPX6',(imsize + 1) / 2,'[pixel] reference pixel along image axis 2')
-        hdu1.header.update('1CRV7P',crval1p,'[pixel] detector coordinate at reference pixel')
-        hdu1.header.update('2CRV7P',crval2p,'[pixel] detector coordinate at reference pixel')
-        hdu1.header.update('1CRPX7',(imsize + 1) / 2,'[pixel] reference pixel along image axis 1')
-        hdu1.header.update('2CRPX7',(imsize + 1) / 2,'[pixel] reference pixel along image axis 2')
-        hdu1.header.update('1CRV8P',crval1p,'[pixel] detector coordinate at reference pixel')
-        hdu1.header.update('2CRV8P',crval2p,'[pixel] detector coordinate at reference pixel')
-        hdu1.header.update('1CRPX8',(imsize + 1) / 2,'[pixel] reference pixel along image axis 1')
-        hdu1.header.update('2CRPX8',(imsize + 1) / 2,'[pixel] reference pixel along image axis 2')
-        hdu1.header.update('1CRV9P',crval1p,'[pixel] detector coordinate at reference pixel')
-        hdu1.header.update('2CRV9P',crval2p,'[pixel] detector coordinate at reference pixel')
-        hdu1.header.update('1CRPX9',(imsize + 1) / 2,'[pixel] reference pixel along image axis 1')
-        hdu1.header.update('2CRPX9',(imsize + 1) / 2,'[pixel] reference pixel along image axis 2')
+        hdu1.header['1CRV4P'] = (crval1p,'[pixel] detector coordinate at reference pixel')
+        hdu1.header['2CRV4P'] = (crval2p,'[pixel] detector coordinate at reference pixel')
+        hdu1.header['1CRPX4'] = ((imsize + 1) / 2,'[pixel] reference pixel along image axis 1')
+        hdu1.header['2CRPX4'] = ((imsize + 1) / 2,'[pixel] reference pixel along image axis 2')
+        hdu1.header['1CRV5P'] = (crval1p,'[pixel] detector coordinate at reference pixel')
+        hdu1.header['2CRV5P'] = (crval2p,'[pixel] detector coordinate at reference pixel')
+        hdu1.header['1CRPX5'] = ((imsize + 1) / 2,'[pixel] reference pixel along image axis 1')
+        hdu1.header['2CRPX5'] = ((imsize + 1) / 2,'[pixel] reference pixel along image axis 2')
+        hdu1.header['1CRV6P'] = (crval1p,'[pixel] detector coordinate at reference pixel')
+        hdu1.header['2CRV6P'] = (crval2p,'[pixel] detector coordinate at reference pixel')
+        hdu1.header['1CRPX6'] = ((imsize + 1) / 2,'[pixel] reference pixel along image axis 1')
+        hdu1.header['2CRPX6'] = ((imsize + 1) / 2,'[pixel] reference pixel along image axis 2')
+        hdu1.header['1CRV7P'] = (crval1p,'[pixel] detector coordinate at reference pixel')
+        hdu1.header['2CRV7P'] = (crval2p,'[pixel] detector coordinate at reference pixel')
+        hdu1.header['1CRPX7'] = ((imsize + 1) / 2,'[pixel] reference pixel along image axis 1')
+        hdu1.header['2CRPX7'] = ((imsize + 1) / 2,'[pixel] reference pixel along image axis 2')
+        hdu1.header['1CRV8P'] = (crval1p,'[pixel] detector coordinate at reference pixel')
+        hdu1.header['2CRV8P'] = (crval2p,'[pixel] detector coordinate at reference pixel')
+        hdu1.header['1CRPX8'] = ((imsize + 1) / 2,'[pixel] reference pixel along image axis 1')
+        hdu1.header['2CRPX8'] = ((imsize + 1) / 2,'[pixel] reference pixel along image axis 2')
+        hdu1.header['1CRV9P'] = (crval1p,'[pixel] detector coordinate at reference pixel')
+        hdu1.header['2CRV9P'] = (crval2p,'[pixel] detector coordinate at reference pixel')
+        hdu1.header['1CRPX9'] = ((imsize + 1) / 2,'[pixel] reference pixel along image axis 1')
+        hdu1.header['2CRPX9'] = ((imsize + 1) / 2,'[pixel] reference pixel along image axis 2')
         outstr.append(hdu1)
 
 # construct output mask bitmap extension
@@ -223,18 +223,18 @@ def keptrim(infile,outfile,kepid,column,row,imsize,clobber,verbose,logfile,statu
         hdu2 = ImageHDU(maskmap)
         for i in range(len(cards2)):
             try:
-                if cards2[i].key not in hdu2.header.keys():
-                    hdu2.header.update(cards2[i].key, cards2[i].value, cards2[i].comment)
+                if cards2[i].keyword not in hdu2.header.keys():
+                    hdu2.header[cards2[i].keyword] = (cards2[i].value, cards2[i].comment)
                 else:
-                    hdu2.header.cards[cards2[i].key].comment = cards2[i].comment
+                    hdu2.header.cards[cards2[i].keyword].comment = cards2[i].comment
             except:
                 pass
-        hdu2.header.update('NAXIS1',imsize,'')
-        hdu2.header.update('NAXIS2',imsize,'')
-        hdu2.header.update('CRVAL1P',crval1p,'[pixel] detector coordinate at reference pixel')
-        hdu2.header.update('CRVAL2P',crval2p,'[pixel] detector coordinate at reference pixel')
-        hdu2.header.update('CRPIX1',(imsize + 1) / 2,'[pixel] reference pixel along image axis 1')
-        hdu2.header.update('CRPIX2',(imsize + 1) / 2,'[pixel] reference pixel along image axis 2')
+        hdu2.header['NAXIS1' ] = (imsize,'')
+        hdu2.header['NAXIS2' ] = (imsize,'')
+        hdu2.header['CRVAL1P'] = (crval1p,'[pixel] detector coordinate at reference pixel')
+        hdu2.header['CRVAL2P'] = (crval2p,'[pixel] detector coordinate at reference pixel')
+        hdu2.header['CRPIX1' ] = ((imsize + 1) / 2,'[pixel] reference pixel along image axis 1')
+        hdu2.header['CRPIX2' ] = ((imsize + 1) / 2,'[pixel] reference pixel along image axis 2')
         outstr.append(hdu2)
 
 # write output file
@@ -245,12 +245,11 @@ def keptrim(infile,outfile,kepid,column,row,imsize,clobber,verbose,logfile,statu
 # close input structure
 
     if status == 0:
-        status = kepio.closefits(instr,logfile,verbose)	    
+        status = kepio.closefits(instr,logfile,verbose)
 
 # end time
 
     kepmsg.clock('KEPTRIM finished at',logfile,verbose)
-
 
 # -------------------------------------
 # KIC retrieval based upon KepID
@@ -272,21 +271,19 @@ def FOVKepID(id):
     lines = urllib.urlopen(url)
     for line in lines:
         line = line.strip()
-        if (len(line) > 0 and 
-            'Kepler' not in line and 
+        if (len(line) > 0 and
+            'Kepler' not in line and
             'integer' not in line and
             'no rows found' not in line):
             out = line.split(',')
-
     return out
-
 
 # -------------------------------------
 # main
 
 if '--shell' in sys.argv:
     import argparse
-    
+
     parser = argparse.ArgumentParser(description=
         'Trim unwanted pixels from a Target Pixel File')
     parser.add_argument('--shell', action='store_true', help='Are we running from the shell?')
@@ -298,22 +295,17 @@ if '--shell' in sys.argv:
     parser.add_argument('--imsize', help='Number of pixels to extract in both row and column dimensions', dest='imsize', type=int)
     parser.add_argument('--clobber', action='store_true', help='Overwrite output file?')
     parser.add_argument('--verbose', action='store_true', help='Write to a log file?')
-    parser.add_argument('--logfile', '-l', help='Name of ascii log file', default='keptrim.log', 
+    parser.add_argument('--logfile', '-l', help='Name of ascii log file', default='keptrim.log',
         dest='logfile', type=str)
     parser.add_argument('--status', '-e', help='Exit status (0=good)', default=0, dest='status', type=int)
 
 
     args = parser.parse_args()
-    
+
     keptrim(args.infile,args.outfile,args.kepid,args.column,args.row,args.imsize,
             args.clobber,args.verbose,args.logfile,args.status)
-    
-    
 
 else:
     from pyraf import iraf
-    
-    
     parfile = iraf.osfn("kepler$keptrim.par")
     t = iraf.IrafTaskFactory(taskname="keptrim", value=parfile, function=keptrim)
-
