e7ffaf01e749c582e7167c508aae249acd304e0b
Ze Vinicius <jvmirca@gmail.com>
2017-06-26 11:02:34 -0700

    change patch color

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -102,7 +102,7 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-html_logo = '_static/images/pyke-patch-red.png'
+html_logo = '_static/images/pyke-patch-black.png'
 
 
 # -- Options for HTMLHelp output ------------------------------------------
