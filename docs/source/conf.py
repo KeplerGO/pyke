a6bcd7fa42546ab802cb726e1456aee7b9dc8377
Geert Barentsen <geert@barentsen.be>
2017-06-30 13:33:30 -0700

    update docs logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -103,7 +103,7 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-html_logo = '_static/images/coollogo_com-577298.gif'
+html_logo = '_static/images/pyke-white.png'
 
 
 # -- Options for HTMLHelp output ------------------------------------------
