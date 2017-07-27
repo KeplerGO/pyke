3bf2618675d4a307e5943d8aa5e39b96a234f163
Ze Vinicius <jvmirca@gmail.com>
2017-07-26 17:36:19 -0700

    force tkagg backend

--- pyke/__init__.py
+++ pyke/__init__.py
@@ -3,6 +3,8 @@
 from __future__ import absolute_import
 import os
 PACKAGEDIR = os.path.abspath(os.path.dirname(__file__))
+import matplotlib
+matplotlib.use('TkAgg')
 
 from .version import __version__
 from .keparray import *
