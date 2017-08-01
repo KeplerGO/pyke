a75509d59db8eaa307ec0ecab21b5ce478f727a3
Christina Hedges <clh93@cam.ac.uk>
2017-08-01 11:57:56 -0700

    Added outfile get_pkg_data_filename for a happy travis

--- pyke/tests/test_kepclip.py
+++ pyke/tests/test_kepclip.py
@@ -5,6 +5,7 @@ import numpy as np
 import os
 TPF_filename  =  get_pkg_data_filename("data/testtpf.fits")
 LC_filename  =  get_pkg_data_filename("data/dip_llc.fits")
+OUT_filename  =  get_pkg_data_filename("data/out.fits")
 
 
 def test_kepclip_lc():
@@ -18,11 +19,11 @@ def test_kepclip_lc():
 	h = fits.open(LC_filename)
 	t = h[1].data['TIME'][200:400]
 	ranges = '{}'.format(t[0] + 2454833) + ', {}'.format(t[-1] + 2454833)
-	kepclip(infile = LC_filename, outfile = 'data/out.fits', ranges = ranges, overwrite = True)
-	j = fits.open('data/out.fits')
+	kepclip(infile = LC_filename, outfile = OUT_filename, ranges = ranges, overwrite = True)
+	j = fits.open(OUT_filename)
 
 	#The new file should be smaller
-	assert os.path.getsize(LC_filename) > os.path.getsize('data/out.fits')
+	assert os.path.getsize(LC_filename) > os.path.getsize(OUT_filename)
 	#The new file should also contain the dip
 	assert np.min(h[1].data['SAP_FLUX']) == np.min(j[1].data['SAP_FLUX'])
 	#The median of the new file should be lower, as we are clipping out the dip.
@@ -43,10 +44,10 @@ def test_kepclip_tpf():
 	t = h[1].data['TIME'][0:2]
 	ranges = '{0:.15f}'.format(t[0] + 2454833) + ', {0:.15f}'.format(t[-1] + 2454833)
 	
-	kepclip(infile = TPF_filename, outfile = 'data/out.fits', ranges = ranges, overwrite = True)
-	j = fits.open('data/out.fits')
+	kepclip(infile = TPF_filename, outfile = OUT_filename, ranges = ranges, overwrite = True)
+	j = fits.open(OUT_filename)
 	#The new file should be smaller
-	assert os.path.getsize(TPF_filename) > os.path.getsize('data/out.fits')
+	assert os.path.getsize(TPF_filename) > os.path.getsize(OUT_filename)
 
 	#Fits files should be the same shape
 	assert len(h) == len(j)
@@ -58,8 +59,8 @@ def test_kepclip_tpf():
 	t = h[1].data['TIME'][4:15]
 	ranges = '{0:.15f}'.format(t[0] + 2454833) + ', {0:.15f}'.format(t[-1] + 2454833)
 	
-	kepclip(infile = TPF_filename, outfile = 'data/out.fits', ranges = ranges, overwrite = True)
-	j = fits.open('data/out.fits')
+	kepclip(infile = TPF_filename, outfile = OUT_filename, ranges = ranges, overwrite = True)
+	j = fits.open(OUT_filename)
 
 	#The middle frame should be identical
 	assert np.allclose(h[1].data['FLUX'][4], j[1].data['FLUX'][0])
