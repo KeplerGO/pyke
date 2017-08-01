f85670148e9baf153408257cd7cd6032bf914462
Ze Vinicius <jvmirca@gmail.com>
2017-08-01 16:37:50 -0700

    fix typo

--- pyke/tests/test_keptrial.py
+++ pyke/tests/test_keptrial.py
@@ -1,7 +1,7 @@
 import pytest
 from astropy.io import fits as pyfits
 from astropy.utils.data import get_pkg_data_filename
-from ..kepbls import keptrial
+from ..keptrial import keptrial
 from ..kepio import delete
 
 # fake light curve with transits of period = 2.02 days
@@ -9,8 +9,8 @@ from ..kepio import delete
 fake_lc = get_pkg_data_filename("data/golden-lc.fits")
 
 def test_keptrial():
-    keptril(fake_lc, "keptrial.fits", datacol='SAP_FLUX', errcol='SAP_FLUX_ERR',
-            fmin=0.4, fmax=0.67, ntrials=5, nfreq=200, overwrite=True)
+    keptrial(fake_lc, "keptrial.fits", datacol='SAP_FLUX', errcol='SAP_FLUX_ERR',
+             fmin=0.4, fmax=0.67, ntrials=5, nfreq=200, overwrite=True)
     f = pyfits.open("keptrial.fits")
     assert abs(f[3].header['PERIOD'] - 2.02) < 0.1
     delete("keptrial.fits", "log_keptrial.txt", False)
