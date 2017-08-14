31a635e182221b779e305c4f779b0d81cc786da4
Ze Vinicius <jvmirca@gmail.com>
2017-08-14 12:29:03 -0700

    changes in kepclip approach

--- pyke/kepclip.py
+++ pyke/kepclip.py
@@ -90,9 +90,6 @@ def kepclip(infile, ranges, outfile=None, datacol='SAP_FLUX', plot=False,
     except:
         cadenom = cadence
 
-    # fudge non-compliant FITS keywords with no values
-    #instr = kepkey.emptykeys(instr, infile, logfile, verbose)
-
     # input data
     table = instr[1].data
 
@@ -104,27 +101,19 @@ def kepclip(infile, ranges, outfile=None, datacol='SAP_FLUX', plot=False,
         flux = flux / cadenom
 
     # filter input data table
-    naxis2 = 0
-    work1 = np.array([], 'float64')
-    work2 = np.array([], 'float32')
-    for i in tqdm(range(len(barytime))):
-        if (np.isfinite(barytime[i]) and np.isfinite(flux[i])
-            and flux[i] != 0.0):
-            reject = False
-            for j in range(len(t1)):
-                if (barytime[i] >= t1[j] and barytime[i] <= t2[j]):
-                    reject = True
-            if not reject:
-                table[naxis2] = table[i]
-                work1 = np.append(work1, barytime[i])
-                work2 = np.append(work2, flux[i])
-                naxis2 += 1
+    reject_time_mask = np.zeros_like(barytime, dtype=bool)
+    for i in range(len(t1)):
+        reject_time_mask += np.array(((barytime >= t1[i]) * (barytime <= t2[i])).reshape(-1))
+    accept_time_mask = ~reject_time_mask
+    work1 = barytime[accept_time_mask]
+    work2 = flux[accept_time_mask]
+    table = table[accept_time_mask]
 
     # comment keyword in output file
     print("Writing output file {}...".format(outfile))
     kepkey.history(call, instr[0], outfile, logfile, verbose)
     # write output file
-    instr[1].data = table[:naxis2]
+    instr[1].data = table
     comment = 'NaN cadences removed from data'
     kepkey.new('NANCLEAN', True, comment, instr[1], outfile, logfile, verbose)
     instr.writeto(outfile)
@@ -191,9 +180,6 @@ def kepclip(infile, ranges, outfile=None, datacol='SAP_FLUX', plot=False,
         plt.xlabel(xlab, {'color' : 'k'})
         plt.ylabel(ylab, {'color' : 'k'})
         plt.grid()
-
-    # render plot
-    if plot:
         plt.show()
 
     # close input file
--- pyke/tests/test_kepclip.py
+++ pyke/tests/test_kepclip.py
@@ -10,9 +10,11 @@ clipped_lc = get_pkg_data_filename("data/golden-lc-clipped.fits")
 
 def test_kepclip():
     kepclip(fake_lc, "2457508.314460751,2457519.5668576737",
-            outfile="kepclip.fits")
+            outfile="kepclip.fits", overwrite=True)
     f = pyfits.open("kepclip.fits")
     g = pyfits.open(clipped_lc)
     assert_array_almost_equal(f[1].data['PDCSAP_FLUX'],
                               g[1].data['PDCSAP_FLUX'])
+    assert_array_almost_equal(f[1].data['TIME'],
+                              g[1].data['TIME'])
     delete("kepclip.fits", "log_kepclip.txt", False)
