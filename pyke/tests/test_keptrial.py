7682e68a5c5dca2fc96d9ee978466296c3ae11e3
Ze Vinicius <jvmirca@gmail.com>
2017-08-25 16:53:36 -0700

    add random state for test_keptrial

--- pyke/tests/test_keptrial.py
+++ pyke/tests/test_keptrial.py
@@ -1,6 +1,7 @@
 import pytest
 from astropy.io import fits as pyfits
 from astropy.utils.data import get_pkg_data_filename
+from astropy.utils.misc import NumpyRNGContext
 from ..keptrial import keptrial
 from ..kepio import delete
 
@@ -9,8 +10,9 @@ from ..kepio import delete
 fake_lc = get_pkg_data_filename("data/golden-lc.fits")
 
 def test_keptrial():
-    keptrial(fake_lc, "keptrial.fits", datacol='SAP_FLUX', errcol='SAP_FLUX_ERR',
-             fmin=0.4, fmax=0.67, ntrials=50, nfreq=10, overwrite=True)
-    f = pyfits.open("keptrial.fits")
-    assert abs(f[3].header['PERIOD'] - 2.02) < 0.1
-    delete("keptrial.fits", "log_keptrial.txt", False)
+    with NumpyRNGContext(123):
+        keptrial(fake_lc, "keptrial.fits", datacol='SAP_FLUX', errcol='SAP_FLUX_ERR',
+                 fmin=0.4, fmax=0.67, ntrials=50, nfreq=10, overwrite=True)
+        f = pyfits.open("keptrial.fits")
+        assert abs(f[3].header['PERIOD'] - 2.02) < 0.1
+        delete("keptrial.fits", "log_keptrial.txt", False)
