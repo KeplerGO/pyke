f6a044e95c1a1bdf69bb57730876c37b37f0cbe3
Ze Vinicius <jvmirca@gmail.com>
2017-09-18 16:38:00 -0700

    WIP: Add KeplerLIghtCurveFile class

--- pyke/lightcurve.py
+++ pyke/lightcurve.py
@@ -1,6 +1,8 @@
 import numpy as np
+from astropy.io import fits
 
-__all__ = ['LightCurve']
+__all__ = ['LightCurve', 'KeplerLightCurveFile']
+
 
 class LightCurve(object):
     """
@@ -14,9 +16,10 @@ class LightCurve(object):
         Data flux for every time point.
     """
 
-    def __init__(self, time, flux):
+    def __init__(self, time, flux, flux_err):
         self.time = time
         self.flux = flux
+        self.flux_err = flux_err
 
     def detrend(self, method='arclength', **kwargs):
         """
@@ -26,8 +29,38 @@ class LightCurve(object):
         else:
             return FirstDifferenceDetrender().detrend(self.time, self.flux, **kwargs)
 
-    def to_pandas(self):
-        raise NotImplementedError("@gully")
+    def draw(self):
+        raise NotImplementedError("Should we implement a LightCurveDrawer class?")
+
+    def to_csv(self):
+        raise NotImplementedError()
+
+    def to_fits(self):
+        raise NotImplementedError()
+
+
+class KeplerLightCurveFile(LightCurve):
+
+    def __init__(self, path, **kwargs):
+        self.hdu = fits.open(path, **kwargs)
+        self.time = self.hdu[1].data['TIME']
+        self.flux = self.get_flux('PDCSAP_FLUX')
+
+    def flux_types(self):
+        """Returns a list of available flux types for this light curve file"""
+        return [n for n in  self.hdu[1].data.columns.names if 'FLUX' in n]
+
+    def get_flux(self, flux_type):
+        if flux_type is in self._flux_types():
+            return self.hdu[1].data[flux_type]
+        else:
+            raise KeyError("{} is not a valid flux type. Available types are: {}".
+                           format(flux_type, self._flux_types))
+
+    def set_flux_type(self, flux_type):
+        self.flux = self.get_flux(flux_type)
+
+
 
 class Detrender(object):
     """
