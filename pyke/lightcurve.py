24134eaab37b0b8a8c1ff1d159d6938ea63a7c6e
Ze Vinicius <jvmirca@gmail.com>
2017-09-18 17:29:22 -0700

    WIP: add get_lightcurve method

--- pyke/lightcurve.py
+++ pyke/lightcurve.py
@@ -16,18 +16,24 @@ class LightCurve(object):
         Data flux for every time point.
     """
 
-    def __init__(self, time, flux, flux_err):
+    def __init__(self, time, flux, flux_err=None, quality=None, centroid_col=None,
+                 centroid_row=None):
         self.time = time
         self.flux = flux
         self.flux_err = flux_err
+        self.quality = quality
+        self.centroid_col = centroid_col
+        self.centroid_row = centroid_row
 
     def detrend(self, method='arclength', **kwargs):
         """
         """
         if method == 'arclength':
-            return ArcLengthDetrender().detrend(self.time, self.flux, **kwargs)
+            return ArcLengthDetrender().detrend(time=self.time, flux=self.flux,
+                                                flux_err=self.flux_err, **kwargs)
         else:
-            return FirstDifferenceDetrender().detrend(self.time, self.flux, **kwargs)
+            return FirstDifferenceDetrender().detrend(time=self.time, flux=self.flux,
+                                                      flux_err=self.flux_err, **kwargs)
 
     def draw(self):
         raise NotImplementedError("Should we implement a LightCurveDrawer class?")
@@ -39,27 +45,32 @@ class LightCurve(object):
         raise NotImplementedError()
 
 
-class KeplerLightCurveFile(LightCurve):
+class KeplerLightCurveFile(object):
 
     def __init__(self, path, **kwargs):
         self.hdu = fits.open(path, **kwargs)
-        self.time = self.hdu[1].data['TIME']
-        self.flux = self.get_flux('PDCSAP_FLUX')
 
-    def flux_types(self):
-        """Returns a list of available flux types for this light curve file"""
-        return [n for n in  self.hdu[1].data.columns.names if 'FLUX' in n]
-
-    def get_flux(self, flux_type):
+    def get_lightcurve(self, flux_type, centroid_type='MOM_CENTR'):
         if flux_type in self._flux_types():
-            return self.hdu[1].data[flux_type]
+            return LightCurve(self.hdu[1].data['TIME'], self.hdu[1].data[flux_type],
+                              flux_err=self.hdu[1].data[flux_type + "_ERR"],
+                              quality=self.hdu[1].data['QUALITY'],
+                              centroid_col=self.hdu[1].data[centroid_type + "1"],
+                              centroid_row=self.hdu[1].data[centroid_type + "2"])
         else:
             raise KeyError("{} is not a valid flux type. Available types are: {}".
                            format(flux_type, self._flux_types))
+    @property
+    def SAP_FLUX(self):
+        return self.get_lightcurve('SAP_FLUX')
 
-    def set_flux_type(self, flux_type):
-        self.flux = self.get_flux(flux_type)
+    @property
+    def PDCSAP_FLUX(self):
+        return self.get_lightcurve('PDCSAP_FLUX')
 
+    def _flux_types(self):
+        """Returns a list of available flux types for this light curve file"""
+        return [n for n in  self.hdu[1].data.columns.names if 'FLUX' in n]
 
 
 class Detrender(object):
