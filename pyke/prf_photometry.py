fix bug in gradients

pyke/prf_photometry.py
@@ -335,13 +335,13 @@ class SimpleKeplerPRF(KeplerPRF):
         delta_row = self.row_coord - center_row
         delta_row, delta_col = np.meshgrid(delta_row, delta_col)
 
-        deriv_flux = self.interpolate(delta_col.flatten(), delta_row.flatten(),
+        deriv_flux = self.interpolate(delta_row.flatten(), delta_col.flatten(),
                                       grid=False).reshape(self.shape)
-        deriv_center_col = - flux * self.interpolate(delta_col.flatten(),
-                                                     delta_row.flatten(), dx=1,
+        deriv_center_col = - flux * self.interpolate(delta_row.flatten(),
+                                                     delta_col.flatten(), dx=1,
                                                      grid=False).reshape(self.shape)
-        deriv_center_row = - flux * self.interpolate(delta_col.flatten(),
-                                                     delta_row.flatten(), dy=1,
+        deriv_center_row = - flux * self.interpolate(delta_row.flatten(),
+                                                     delta_col.flatten(), dy=1,
                                                      grid=False).reshape(self.shape)
         return [deriv_flux, deriv_center_col, deriv_center_row]
 