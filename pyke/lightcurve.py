9587b0636169ab6f38ad74ddadaeda8ba104a581
Geert Barentsen <geert@barentsen.be>
2018-01-03 11:40:01 -0800

    @barentsen broke master, undo the damage

--- pyke/lightcurve.py
+++ pyke/lightcurve.py
@@ -119,8 +119,9 @@ class LightCurve(object):
         """
         fold_time = ((self.time - phase + 0.5 * period) / period) % 1 - 0.5
         sorted_args = np.argsort(fold_time)
-        return LightCurve(fold_time[sorted_args], self.flux[sorted_args],
-                          flux_err=self.flux_err[sorted_args])
+        if self.flux_err is None:
+            return LightCurve(fold_time[sorted_args], self.flux[sorted_args])
+        return LightCurve(fold_time[sorted_args], self.flux[sorted_args], flux_err=self.flux_err[sorted_args])
 
     def remove_nans(self):
         """Removes cadences where the flux is NaN.
