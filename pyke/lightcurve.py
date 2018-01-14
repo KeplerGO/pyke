7b066023938b51ad4d7dceb87bfe4c519671f654
Geert Barentsen <geert@barentsen.be>
2018-01-14 15:15:08 -0800

    Implement bin() using np.array_split

--- pyke/lightcurve.py
+++ pyke/lightcurve.py
@@ -167,13 +167,15 @@ class LightCurve(object):
             new_lc.flux_err = self.flux_err[~outlier_mask]
         return new_lc
 
-    def bin(self, binsize=13):
+    def bin(self, binsize=13, method='mean'):
         """Bins a lightcurve using a block mean of size `binsize`.
 
         Parameters
         ----------
         binsize : int
-            Size of the bin
+            Number of cadences to include in every bin.
+        method: str, one of 'mean' or 'median'
+            The summary statistic to return for each bin. Default: 'mean'.
 
         Returns
         -------
@@ -182,20 +184,35 @@ class LightCurve(object):
 
         Notes
         -----
-            If the ratio between the lightcurve length and the binsize is not
-            a whole number, then the remainder of the data points will be
-            ignored.
+        - If the ratio between the lightcurve length and the binsize is not
+          a whole number, then the remainder of the data points will be
+          ignored.
+        - If the original lightcurve contains flux uncertainties (flux_err),
+          the binned lightcurve will report the root-mean-square error.
+          If no uncertainties are included, the binned curve will return the
+          standard deviation of the data.
         """
+        available_methods = ['mean', 'median']
+        if method not in available_methods:
+            raise ValueError("method must be one of: {}".format(available_methods))
+        methodf = np.__dict__['nan' + method]
+
+        n_bins = self.flux.size // binsize
         binned_lc = copy.copy(self)
-        q = len(self.flux) % binsize
-        flux = self.flux[:-q or None].reshape(-1, binsize)
-        time = self.time[:-q or None].reshape(-1, binsize)
-        binned_lc.time = np.nanmean(time, axis=-1)
-        binned_lc.flux = np.nanmean(flux, axis=-1)
+        binned_lc.time = np.array([methodf(a) for a in np.array_split(self.time, n_bins)])
+        binned_lc.flux = np.array([methodf(a) for a in np.array_split(self.flux, n_bins)])
 
         if self.flux_err is not None:
-            flux_err = self.flux_err[:-q or None].reshape(-1, binsize)
-            binned_lc.flux_err = np.sqrt(np.nansum(flux_err ** 2, axis=-1)) / binsize
+            # root-mean-square error
+            binned_lc.flux_err = np.array(
+                                    [np.sqrt(np.nansum(a**2))
+                                     for a in np.array_split(self.flux_err, n_bins)]
+                                  ) / binsize
+        else:
+            # compute the standard deviation from the data
+            binned_lc.flux_err = np.array([np.nanstd(a)
+                                           for a in np.array_split(self.flux, n_bins)])
+
         return binned_lc
 
     def cdpp(self, transit_duration=13, savgol_window=101, savgol_polyorder=2,
